a5ca4d7aedb828613d5987d291faa5787fc41b34
JuanM04 <me@juanm04.com>
2022-12-13 10:53:31 -0300

    🎨 Added Prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+/** @type{import('prettier').Config} */
+module.exports = {
+  printWidth: 100,
+  arrowParens: "avoid",
+  trailingComma: "all",
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  plugins: [
+    require.resolve("prettier-plugin-organize-imports"),
+    require.resolve("prettier-plugin-tailwindcss"), // MUST come last
+  ],
+  pluginSearchDirs: false,
+  tailwindConfig: "./tailwind.config.cjs",
+};
