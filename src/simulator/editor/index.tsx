2c43ce6e13b47b77cc930c5ad11e0228b24316af
JuanM04 <me@juanm04.com>
2022-12-27 19:05:03 -0300

    📝 Better welcome code

--- src/simulator/editor/index.tsx
+++ src/simulator/editor/index.tsx
@@ -46,7 +46,7 @@ export function Editor() {
 
     window.codemirror = new EditorView({
       state: EditorState.create({
-        doc: example,
+        doc: initialCode,
         extensions: [
           EditorState.tabSize.of(2),
 
@@ -121,84 +121,29 @@ function errorsToText(errors: ErrorsStore) {
     : `Hay ${errors.numberOfErrors} errores. Solucionalos antes de compilar.`;
 }
 
-const example = `                   ORG 1000h
-                   nombre: DB "JuanLorenzoMartin"
-                   fin_nombre: DB 00h
-                   vocales: DB "AEIOUaeiou"
-                   fin_vocales: DB 00h
-                   cantidad_vocales: DB 0
+const initialCode = `; ¡Bienvenido a VonSim!
+; Este es un ejemplo de código que calcula los primeros
+; n números de la sucesión de Fibonacci, y se guardan a
+; partir de la posición 1000h de la memoria.
 
-                   ORG 3000h
-                   ; CX es la referencia al primer valor
-                   ; DX es la referencia al segundo valor
-             SWAP: PUSH AX
-                   PUSH BX
-                   MOV BX, CX
-                   MOV AL, [BX] ; Primer valor en AL
-                   MOV BX, DX
-                   MOV AH, [BX] ; Segundo valor en AH
-                   MOV [BX], AL ; Primer valor --> segunda referencia
-                   MOV BX, CX
-                   MOV [BX], AH ; Segundo valor --> primera referencia
-                   POP BX
-                   POP AX
-                   RET
+n:      EQU 10    ; Calcula los primeros 10 números
 
-                   ; Caracter por pila
-                   ; Si es vocal, AH=FFh; de lo contrario, AH=00h
-    ES_VOCAL:      PUSH BX
-                   MOV BX, SP
-                   ADD BX, 2 + 2
-                   MOV AL, [BX] ; Caracter en AL
-                   MOV BX, OFFSET vocales - 1
-                   MOV AH, 00h
-    ES_VOCAL_LOOP: INC BX
-                   CMP BX, OFFSET fin_vocales
-                   JZ ES_VOCAL_FIN
-                   CMP AL, [BX]
-                   JNZ ES_VOCAL_LOOP
-                   MOV AH, 0FFh
-    ES_VOCAL_FIN:  POP BX
-                   RET
-                   ; Nota sobre el INC BX:
-                   ; Como no podemos usar PUSHF ni POPF porque
-                   ; no están bien implementados, tenemos que
-                   ; poner el INC en el inicio del loop.
+        ORG 1000h
+inicio: DB 1
 
+        ORG 2000h
+        MOV BX, OFFSET inicio + 1
+        MOV AL, 0
+        MOV AH, inicio
 
-                   ORG 2000h
-                   MOV CX, OFFSET nombre
-                   MOV DX, OFFSET fin_nombre - 1
-        MAIN_LOOP: CMP DX, CX
-                   JS LOOP_FIN
-                   ; Contar (o no) si el valor al que
-                   ; apunta en CX es vocal
-                   MOV BX, CX
-                   MOV AL, [BX]
-                   MOV AH, 00
-                   PUSH AX
-                   CALL ES_VOCAL
-                   CMP AH, 0
-                   JZ NO_CONTAR_1
-                   INC cantidad_vocales
-      NO_CONTAR_1: POP AX
-                   ; Si CX y DX apuntan a la misma letra, terminar
-                   CMP DX, CX
-                   JZ LOOP_FIN
-                   ; Contar (o no) si el valor al que
-                   ; apunta en DX es vocal
-                   MOV BX, DX
-                   MOV AL, [BX]
-                   MOV AH, 0
-                   PUSH AX
-                   CALL ES_VOCAL
-                   CMP AH, 0
-                   JZ NO_CONTAR_2
-                   INC cantidad_vocales
-      NO_CONTAR_2: POP AX
-                   ; Swapear
-                   CALL SWAP
-                   INC CX
-                   DEC DX
-                   JMP MAIN_LOOP
-         LOOP_FIN: HLT`;
+ BUCLE: CMP BX, OFFSET inicio + n
+        JNS FIN
+        MOV CL, AH
+        ADD CL, AL
+        MOV AL, AH
+        MOV AH, CL
+        MOV [BX], CL
+        INC BX
+        JMP BUCLE
+   FIN: HLT
+`;
