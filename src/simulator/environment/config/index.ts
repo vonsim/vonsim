5ef7f6e911e42d939d2aedcddb9de06c3b1c0368
JuanM04 <me@juanm04.com>
2022-12-29 13:51:09 -0300

    🗃️ Persist user config

--- src/simulator/environment/config/index.ts
+++ src/simulator/environment/config/index.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 export type MemoryRepresentation = "hex" | "bin" | "int" | "uint" | "ascii";
 export type MemoryOnReset = "empty" | "random" | "keep";
@@ -12,11 +13,20 @@ export type ConfigStore = {
   setClockSpeed: (speed: number) => void;
 };
 
-export const useConfig = create<ConfigStore>()(set => ({
-  memoryRepresentation: "hex",
-  memoryOnReset: "random",
-  clockSpeed: 1,
-  setMemoryRepresentation: representation => set({ memoryRepresentation: representation }),
-  setMemoryOnReset: mode => set({ memoryOnReset: mode }),
-  setClockSpeed: speed => set({ clockSpeed: speed }),
-}));
+export const useConfig = create<ConfigStore>()(
+  persist(
+    set => ({
+      memoryRepresentation: "hex",
+      memoryOnReset: "random",
+      clockSpeed: 1,
+      setMemoryRepresentation: representation => set({ memoryRepresentation: representation }),
+      setMemoryOnReset: mode => set({ memoryOnReset: mode }),
+      setClockSpeed: speed => set({ clockSpeed: speed }),
+    }),
+    {
+      name: "userconfig",
+      version: 0,
+      getStorage: () => localStorage,
+    },
+  ),
+);
