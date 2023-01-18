4112e4b3fd95ccd36455a51cf1dba46bff3bb947
JuanM04 <me@juanm04.com>
2023-01-18 19:58:50 -0300

    ✨ Added interrupts to handshake

--- src/simulator/devices/handshake.ts
+++ src/simulator/devices/handshake.ts
@@ -14,7 +14,7 @@ export const createHandshakeSlice: DeviceSlice<HandshakeSlice> = (set, get) => (
   devices: {
     handshake: {
       data: 0,
-      state: 0, // XXXX XXSB, where S is strobe, B is busy
+      state: 0, // IXXX XXSB, where I is interrupt, S is strobe, B is busy
       riseStrobe: false,
 
       setData: data =>
@@ -32,6 +32,13 @@ export const createHandshakeSlice: DeviceSlice<HandshakeSlice> = (set, get) => (
             state.devices.handshake.state |= 0b10; // set strobe to 1
             state.devices.handshake.riseStrobe = false;
           });
+        }
+
+        const interruptEnabled = (get().devices.handshake.state & 0b10000000) !== 0;
+        const printerIsReady = (get().devices.handshake.state & 0b1) === 0;
+        if (interruptEnabled && printerIsReady) {
+          // Hanshaked is linked to INT2
+          get().devices.pic.request(2);
         }
       },
     },
