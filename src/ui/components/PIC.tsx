d2c5c9499721f4f623da7492f9a8ad55260e59f4
JuanM04 <me@juanm04.com>
2023-01-18 19:42:42 -0300

    💄 PIC has more info now

--- src/ui/components/PIC.tsx
+++ src/ui/components/PIC.tsx
@@ -8,32 +8,53 @@ export function PIC({ className }: { className?: string }) {
 
   return (
     <Card title="PIC" className={className}>
-      <table>
-        <thead>
-          <tr className="divide-x border-b">
-            <th className="px-2 text-center font-bold text-slate-800">EOI</th>
-            <th className="px-2 text-center font-bold text-slate-800">IMR</th>
-            <th className="px-2 text-center font-bold text-slate-800">IRR</th>
-            <th className="px-2 text-center font-bold text-slate-800">ISR</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y">
-          <tr className="divide-x">
-            <td className="px-2 text-center font-mono text-slate-600">
-              {renderMemoryCell(pic.EOI, "bin")}
-            </td>
-            <td className="px-2 text-center font-mono text-slate-600">
-              {renderMemoryCell(pic.IMR, "bin")}
-            </td>
-            <td className="px-2 text-center font-mono text-slate-600">
-              {renderMemoryCell(pic.IRR, "bin")}
-            </td>
-            <td className="px-2 text-center font-mono text-slate-600">
-              {renderMemoryCell(pic.ISR, "bin")}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="flex flex-wrap gap-4">
+        <Card title="Estado">
+          <table>
+            <tbody className="divide-y">
+              <tr className="divide-x">
+                <td className="px-2 text-center font-bold text-slate-800">EOI</td>
+                <td className="px-2 text-center font-mono text-slate-600">
+                  {renderMemoryCell(pic.EOI, "bin")}
+                </td>
+              </tr>
+              <tr className="divide-x">
+                <td className="px-2 text-center font-bold text-slate-800">IMR</td>
+                <td className="px-2 text-center font-mono text-slate-600">
+                  {renderMemoryCell(pic.IMR, "bin")}
+                </td>
+              </tr>
+              <tr className="divide-x">
+                <td className="px-2 text-center font-bold text-slate-800">IRR</td>
+                <td className="px-2 text-center font-mono text-slate-600">
+                  {renderMemoryCell(pic.IRR, "bin")}
+                </td>
+              </tr>
+              <tr className="divide-x">
+                <td className="px-2 text-center font-bold text-slate-800">ISR</td>
+                <td className="px-2 text-center font-mono text-slate-600">
+                  {renderMemoryCell(pic.ISR, "bin")}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Card>
+
+        <Card title="Conexiones">
+          <table>
+            <tbody className="divide-y">
+              {Array.from({ length: 8 }).map((_, i) => (
+                <tr className="divide-x" key={i}>
+                  <td className="px-2 text-center font-bold text-slate-800">INT{i}</td>
+                  <td className="px-2 text-center font-mono text-slate-600">
+                    {renderMemoryCell(pic[`INT${i as 0}`], "uint")}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Card>
+      </div>
     </Card>
   );
 }
