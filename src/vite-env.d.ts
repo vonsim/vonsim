8807791833ddf95338c043816f929f8bfbfa84ca
JuanM04 <me@juanm04.com>
2023-02-09 14:35:56 -0300

    📱 PWA

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/client" />
 
 interface Window {
   codemirror: import("@codemirror/view").EditorView | null;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: "mpa",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["fonts/**/*", "favicon.svg", "icon-192.png", "icon-512.png"],
+      manifest: {
+        name: "VonSim",
+        short_name: "VonSim",
+        description: "A 8088-like Assembly Simulator",
+        theme_color: "#38bdf8",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          { src: "icon-192.png", type: "image/png", sizes: "192x192" },
+          { src: "icon-512.png", type: "image/png", sizes: "512x512" },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: { "@/": "/src/" },
   },
