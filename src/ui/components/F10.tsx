6559843f453a910d8d4b2de3f03ad1751a0a857e
JuanM04 <me@juanm04.com>
2023-03-01 19:39:04 -0300

    💄 Big red interrupt button

--- src/ui/components/F10.tsx
+++ src/ui/components/F10.tsx
@@ -1,13 +1,19 @@
 import { useKey } from "react-use";
 
+import { bit } from "@/helpers";
 import { Card } from "@/ui/components/common/Card";
 import { useSimulator } from "@/ui/hooks/useSimulator";
 import { useTranslate } from "@/ui/hooks/useTranslate";
 
+import styles from "./F10.module.css";
+
 export function F10({ className }: { className?: string }) {
   const translate = useTranslate();
 
-  const dispatch = useSimulator(s => s.dispatch);
+  const { dispatch, disabled } = useSimulator(s => ({
+    disabled: bit(s.simulator.devices.pic.IMR, 0),
+    dispatch: s.dispatch,
+  }));
 
   useKey("F10", ev => {
     ev.preventDefault();
@@ -17,14 +23,14 @@ export function F10({ className }: { className?: string }) {
   return (
     <Card title={translate("devices.external.f10.name")} className={className}>
       <button
-        className="
-          flex h-full items-center justify-center rounded-b-lg border border-sky-400 p-2 transition
-          hover:bg-sky-400 hover:text-white
-          focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-400
-        "
+        className={styles.pushable}
+        disabled={disabled}
         onClick={() => dispatch("f10.press")}
+        title="F10"
       >
-        {translate("devices.external.f10.interrupt")}
+        <div className={styles.shadow} />
+        <div className={styles.edge} />
+        <span className={styles.front}>{translate("devices.external.f10.interrupt")}</span>
       </button>
     </Card>
   );
