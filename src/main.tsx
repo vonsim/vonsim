f493903ac6653adcd37cf458f8cc2839d955e3e3
JuanM04 <me@juanm04.com>
2023-03-22 08:50:25 -0300

    🚸 Added prompt to reload

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,24 @@ import "@/ui/styles/index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Toaster } from "sonner";
+import { toast, Toaster } from "sonner";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "@/ui/App";
 
 // Initialize CodeMirror as null
 window.codemirror = null;
+
+registerSW({
+  onNeedRefresh: () => {
+    toast("There's a new version available!", {
+      action: {
+        label: "Update",
+        onClick: () => window.location.reload(),
+      },
+    });
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="@total-typescript/ts-reset" />
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pwa/vanillajs" />
 
 interface Window {
   codemirror: import("@codemirror/view").EditorView | null;
