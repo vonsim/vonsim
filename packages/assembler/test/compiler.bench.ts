0aa1a0af84f94e6180204e26c40c18c160b384fc
JuanM04 <me@juanm04.com>
2023-05-10 00:21:34 -0300

    ✅ Fixtures

---
+++ packages/assembler/test/compiler.bench.ts
@@ -0,0 +1,16 @@
+import { readdir, readFile } from "node:fs/promises";
+
+import { bench, describe } from "vitest";
+
+import { compile } from "@/index";
+
+describe("Compiler benchmarks", async () => {
+  const path = new URL("./fixtures/", import.meta.url);
+  const files = await readdir(path);
+
+  for (const file of files) {
+    const source = await readFile(new URL(file, path), { encoding: "utf-8" });
+
+    bench(file, () => void compile(source));
+  }
+});
---
+++ packages/assembler/test/compiler.test.ts
@@ -0,0 +1,17 @@
+import { readdir, readFile } from "node:fs/promises";
+
+import { describe, expect, it } from "vitest";
+
+import { compile } from "@/index";
+
+describe("Fixtures", async () => {
+  const path = new URL("./fixtures/", import.meta.url);
+  const files = await readdir(path);
+
+  for (const file of files) {
+    it(`should match snapshot of ${file}`, async () => {
+      const source = await readFile(new URL(file, path), { encoding: "utf-8" });
+      expect(compile(source)).toMatchSnapshot();
+    });
+  }
+});
