20fe5d717d79a307b30879bda30b31dc661028b9
JuanM04 <me@juanm04.com>
2023-07-15 00:12:54 -0300

    🔍️ Added site to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vonsim.github.io",
+  base: "/docs",
+
   integrations: [
     starlight({
       title: "VonSim",
