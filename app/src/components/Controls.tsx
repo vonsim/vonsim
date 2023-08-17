5c9822f51e233a0045c887e61a98b3761c7f096a
JuanM04 <me@juanm04.com>
2023-08-17 17:11:47 -0300

    🚸 Added shortcuts

--- app/src/components/Controls.tsx
+++ app/src/components/Controls.tsx
@@ -1,4 +1,6 @@
 import clsx from "clsx";
+import { useCallback } from "react";
+import { useKey } from "react-use";
 
 import {
   DropdownMenu,
@@ -11,8 +13,35 @@ import { useTranslate } from "@/lib/i18n";
 
 export function Controls({ className }: { className?: string }) {
   const translate = useTranslate();
-
   const { status, dispatch } = useSimulation();
+
+  const runCycle = useCallback(() => {
+    if (status.type === "running") return;
+    dispatch("cpu.run", "cycle-change");
+  }, [status, dispatch]);
+
+  const runInstruction = useCallback(() => {
+    if (status.type === "running") return;
+    dispatch("cpu.run", "end-of-instruction");
+  }, [status, dispatch]);
+
+  const runInfinity = useCallback(() => {
+    if (status.type === "running") return;
+    dispatch("cpu.run", "infinity");
+  }, [status, dispatch]);
+
+  useKey("F7", ev => {
+    ev.preventDefault();
+    runCycle();
+  });
+  useKey("F8", ev => {
+    ev.preventDefault();
+    runInstruction();
+  });
+  useKey("F4", ev => {
+    ev.preventDefault();
+    runInfinity();
+  });
 
   return (
     <div className={clsx("flex items-center justify-center gap-4", className)}>
@@ -20,7 +49,6 @@ export function Controls({ className }: { className?: string }) {
         <DropdownMenuTrigger asChild>
           <button
             disabled={status.type === "running"}
-            onClick={() => dispatch("cpu.run", "infinity")}
             className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md bg-mantis-500 px-3 text-sm text-white ring-offset-stone-900 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-mantis-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
           >
             <span className="icon-[lucide--play] mr-2 h-4 w-4" />
@@ -30,26 +58,23 @@ export function Controls({ className }: { className?: string }) {
           </button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="start">
-          <DropdownMenuItem
-            disabled={status.type === "running"}
-            onClick={() => dispatch("cpu.run", "cycle-change")}
-          >
+          <DropdownMenuItem disabled={status.type === "running"} onClick={runCycle}>
             <span className="icon-[lucide--chevron-right] mr-2 h-4 w-4" />
             {translate("control.action.run.cycle-change")}
+            <div className="grow" />
+            <kbd className="text-stone-600">F7</kbd>
           </DropdownMenuItem>
-          <DropdownMenuItem
-            disabled={status.type === "running"}
-            onClick={() => dispatch("cpu.run", "end-of-instruction")}
-          >
+          <DropdownMenuItem disabled={status.type === "running"} onClick={runInstruction}>
             <span className="icon-[lucide--chevrons-right] mr-2 h-4 w-4" />
             {translate("control.action.run.end-of-instruction")}
+            <div className="grow" />
+            <kbd className="text-stone-600">F8</kbd>
           </DropdownMenuItem>
-          <DropdownMenuItem
-            disabled={status.type === "running"}
-            onClick={() => dispatch("cpu.run", "infinity")}
-          >
+          <DropdownMenuItem disabled={status.type === "running"} onClick={runInfinity}>
             <span className="icon-[lucide--infinity] mr-2 h-4 w-4" />
             {translate("control.action.run.infinity")}
+            <div className="grow" />
+            <kbd className="text-stone-600">F4</kbd>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
